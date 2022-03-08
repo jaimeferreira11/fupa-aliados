b158700c3dc41b3df0ab2d5cbf6dcd50d82d9147
jaimeferreira11 <jaimeferreira11@gmail.com>
2022-03-08 16:23:56 -0300

    appcenter

--- lib/app/config/constants.dart
+++ lib/app/config/constants.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/foundation.dart';
+
 class AppConstants {
-  static const API_URL = 'https://apps.fundacionparaguaya.org.py/fupapp-rest/';
-  //static const API_URL = 'http://10.0.10.11:8080/fupapp-rest/';
+  static const API_URL = kReleaseMode
+      ? 'https://apps.fundacionparaguaya.org.py/fupapp-rest/'
+      : 'http://10.0.10.11:8080/fupapp-rest/';
 
   static const CLIENT_SECRET = 'clientAliados:secret';
 
